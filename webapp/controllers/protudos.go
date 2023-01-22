7e1bca08a246b0d1228b1539cc1d1a21df233dbe
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-01-22 15:16:55 -0300

    Update new page and mods

--- webapp/controllers/protudos.go
+++ webapp/controllers/protudos.go
@@ -10,6 +10,10 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos
-	temp.ExecuteTemplate(w, "index", produtos)
+	temp.ExecuteTemplate(w, "Index", todosOsprodutos)
 
+}
+
+func New(w http.ResponseWriter, r *http.Request){
+	temp.ExecuteTemplate(w, "New", nil)
 }
