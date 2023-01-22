a7ffbcdabb2d6d913bdee61c887b8a6250458874
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-01-21 22:25:51 -0300

    Updating db connection

--- webapp/main.go
+++ webapp/main.go
@@ -3,7 +3,17 @@ package main
 import (
 	"html/template"
 	"net/http"
+	_ "github.com/lib/pq"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_lojas password=12345678 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -15,6 +25,8 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := conectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
