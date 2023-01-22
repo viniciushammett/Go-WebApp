0288b8e1b66353c9af6ccf82a8bedc2c1e74c3ef
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-01-22 14:37:53 -0300

    Updating variables

--- webapp/main.go
+++ webapp/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"html/template"
 	"net/http"
+	"github.com/viniciushammett/models"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -14,8 +15,7 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	
+	todosOsProdutos := models.BuscaTodosOsProdutos
 	temp.ExecuteTemplate(w, "index", produtos)
-	defer db.Close()
 
 }
